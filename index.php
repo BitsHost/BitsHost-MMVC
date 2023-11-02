11a68ecef0f073ef3760eabd53e64663d991d299
BitsHost <dadryan26@yahoo.com>
2023-11-02 21:47:28 +0200

    up

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
  * Created on Tue Oct 31 2023
  *
  * Copyright (c) 2023 BitsHost
+ * 
+ * Here you may host your app for free:
+ * https://bitshost.biz/
  */
 
 
@@ -23,9 +26,9 @@ $_SESSION["username"] = "defaultAppUser";
 /////////////////////////////////////
 
 define('THIS_DIR', str_replace("\\", "/", dirname(__FILE__, 2)));
-define('BASE_URL', 'https://yoursite.com');
+define('BASE_URL', 'http://localhost/crud/MVC/mvc');
 
-$sitePath = "";
+$sitePath = "/crud/MVC/mvc";
 //should be empty if domain location is root; e.g. public_html
 $url = $_SERVER['REQUEST_URI'];
 $url = str_replace($sitePath, "", $url);
